9a60cdd9fa286191704535562ebb068d1e0a5d67
Break <franklinbacacampos@gmail.com>
2026-01-14 21:37:24 -0500

    nuevo archivo para el modulo de blending

---
+++ blending.php
@@ -0,0 +1,159 @@
+<?php
+// Inicia la sesión
+session_start();
+
+// Inclusión de archivos de configuración y utilidades
+include('cnx/cnx.php');
+include('global/variables.php');
+include('global/auxiliares.php');
+
+// Redirección si el usuario no está autenticado
+if (!isset($_SESSION["Id"])) {
+  header('Location: index.php');
+  exit;
+}
+
+// endpoint de backend
+$backendUrl = 'apis/backend.php';
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" href="<?php echo $favicon; ?>" type="image/png" />
+
+  <title><?php echo $nom_app; ?> | Blending</title>
+
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
+
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" />
+  <link rel="stylesheet"
+    href="https://cdn.jsdelivr.net/npm/select2-bootstrap-5-theme@1.3.0/dist/select2-bootstrap-5-theme.min.css" />
+
+  <link rel="stylesheet" href="<?php echo $url_lims; ?>/global/styles.css">
+
+  <style>
+
+  </style>
+</head>
+
+<body class="bg-light" style="zoom: 80%;">
+  <div class="container-fluid">
+    <div class="row">
+      <?php echo $navbar_maintop; ?>
+
+      <div class="modal fade" id="menuModal" tabindex="-1" aria-labelledby="menuModalLabel" aria-hidden="true"
+        data-bs-backdrop="static" data-bs-keyboard="false">
+        <div class="modal-dialog modal-left" style="margin-top: 0px !important; margin-left: 0px !important;">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="menuModalLabel">Menú de Opciones</h5>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body"
+              style="background: #25476a; color: white; border-top: solid #EFB810 3px; padding: 0px !important;">
+              <ul class="list-unstyled">
+                <div id="div_menu1"></div>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="col-md-12 col-sm-12 col-xs-12" style="padding-top: 10px; padding-left: 15px; padding-right: 15px;">
+        <div class="d-flex row">
+
+
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <?php include('global/auxiliares_js.php'); ?>
+
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"
+    integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa"
+    crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/echarts@5.3.3/dist/echarts.min.js"></script>
+
+
+  <script type="text/javascript">
+    document.addEventListener("DOMContentLoaded", function () {
+
+      // -------------------------
+      // Variables Globales
+      // -------------------------
+
+      // -------------------------
+      // Funciones Auxiliares de Datos
+      // -------------------------
+
+      function f_callBackend(accion, data) {
+        return $.post(backendUrl, {
+          accion: accion,
+          ...data
+        }, 'json');
+      }
+
+      function findProveedorById(id) {
+        return proveedores_con_anticipos.find(p => String(p.id_proveedor) === String(id));
+      }
+
+      // -------------------------
+      // Lógica de Renderizado
+      // -------------------------
+
+
+      // -------------------------
+      // Lógica de Eventos
+      // -------------------------
+
+      // 1. Carga Inicial de Datos
+      function loadAllData() {
+        f_callBackend('', {})
+          .done(function (r) {
+            if (r.estado === 1 && r.data) {
+
+
+            } else {
+              alert("Error al cargar datos iniciales: " + (r.msg || "Respuesta incompleta del servidor."));
+            }
+          })
+          .fail(function () {
+            alert("Error de conexión al cargar datos de anticipos.");
+          });
+      }
+
+
+
+      // -------------------------
+      // Start
+      // -------------------------
+
+      function f_Init() {
+        // Genera menús
+        f_GetMenuPrincipal();
+
+        // Titulo de Pantalla
+        $("#nv_titulo").html('| Anticipos a proveedores');
+
+        // Carga todos los datos al iniciar
+        //loadAllData();
+      }
+
+      f_Init();
+    });
+  </script>
+
+</body>
+
+</html>
